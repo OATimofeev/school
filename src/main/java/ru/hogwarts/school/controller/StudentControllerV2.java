fe47cc8c56c0bf7380d1941e88241ef3b4ef88bc
Oleg Timofeev <oleg.timofeev@rtmis.ru>
2026-05-12 17:16:52 +0300

    ДЗ: Потоки

--- src/main/java/ru/hogwarts/school/controller/StudentControllerV2.java
+++ src/main/java/ru/hogwarts/school/controller/StudentControllerV2.java
@@ -27,4 +27,16 @@ public class StudentControllerV2 {
         return ResponseEntity.ok(studentService.getAverageAgeByStreams());
     }
 
+    @GetMapping("/print-parallel")
+    public ResponseEntity printParallel() {
+        studentService.printParallel();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/print-synchronized")
+    public ResponseEntity printSynchronized() {
+        studentService.printSynchronized();
+        return ResponseEntity.ok().build();
+    }
+
 }
